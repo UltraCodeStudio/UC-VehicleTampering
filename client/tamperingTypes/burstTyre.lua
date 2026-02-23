6121200c8231c7b3e042527b45ebad9987bcba85
DJultra14 <callumglasspool@gmail.com>
2026-02-23 12:40:52 +0000

    tyre Burst

--- client/tamperingTypes/burstTyre.lua
+++ client/tamperingTypes/burstTyre.lua
@@ -2,5 +2,5 @@ function burstRandomTyre(veh)
     local wheels = GetVehicleNumberOfWheels(veh)
     if not wheels or wheels <= 0 then return -1 end
 
-    BreakOffVehicleWheel(veh, math.random(0, wheels - 1), false)
+    SetVehicleTyreBurst(veh, math.random(0, wheels - 1), true, 1000.0)
 end
